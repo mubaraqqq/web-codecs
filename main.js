4dfb3479c71b6d887eef108b67082fc042e74996
Mubaraq Mustapha <ayomidemusty@gmail.com>
2024-11-24 14:16:09 +0100

    feat: implement encoding

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ import { setupCounter } from "./counter.js";
 const videoInput = document.querySelector('input[name="video-input"]');
 const videoEl = document.createElement("video");
 const body = document.querySelector("body");
+let frameCounter = 0;
+
+const encoder = new VideoEncoder({
+  output(chunk, metadata) {
+    const chunkData = new Uint8Array(chunk.byteLength);
+    console.log({ chunkData });
+    chunk.copyTo(chunkData);
+
+    console.log({ chunk, chunkData });
+  },
+  error(error) {
+    console.log(error);
+  },
+});
+
+let config = {
+  codec: "vp8",
+  width: 640,
+  height: 480,
+  bitrate: 2_000_000, // 2 Mbps
+  framerate: 30,
+};
+
+encoder.configure(config);
 
 console.log(videoInput);
 
@@ -22,6 +46,7 @@ videoInput.addEventListener("change", (e) => {
   videoEl.height = 200;
   videoEl.width = 200;
   body.appendChild(videoEl);
+  frameCounter = 0;
 });
 
 videoEl.addEventListener("timeupdate", () => {
@@ -29,7 +54,15 @@ videoEl.addEventListener("timeupdate", () => {
     timestamp: videoEl.currentTime * 1000,
   });
 
-  console.log(videoFrame);
+  if (encoder.encodeQueueSize > 2) {
+    videoFrame.close();
+  } else {
+    frameCounter++;
+    const keyFrame = frameCounter % 150 === 0;
+    encoder.encode(videoFrame, { keyFrame });
+  }
+
+  console.log(frameCounter);
 
   videoFrame.close();
 });
